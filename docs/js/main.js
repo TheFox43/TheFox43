7d27a7bd2f15282c95bfd214af4b250397633604
Francesco Volpe <francesco.volpe1998@gmail.com>
2026-03-27 11:12:13 +0100

    small details, email in the footer, email icon, animations, rounded profile pic etc

--- docs/js/main.js
+++ docs/js/main.js
@@ -2,19 +2,17 @@ document.addEventListener("DOMContentLoaded", () => {
     const toggle = document.getElementById("menu-toggle");
     const menu = document.getElementById("menu");
 
-    // Toggle apertura/chiusura
     toggle.addEventListener("click", (e) => {
         e.preventDefault();
         menu.classList.toggle("active");
     });
 
-    // Chiusura cliccando fuori
     document.addEventListener("click", (e) => {
         const isClickInsideMenu = menu.contains(e.target);
         const isClickOnToggle = toggle.contains(e.target);
 
         if (!isClickInsideMenu && !isClickOnToggle) {
-        menu.classList.remove("active");
+            menu.classList.remove("active");
         }
     });
 });
